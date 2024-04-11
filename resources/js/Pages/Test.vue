277e3f6313aac6298231529d748bc736d2e84203
Thoss24 <t.fogarty24@outlook.com>
2024-04-11 19:13:07 +0100

    setup

---
+++ resources/js/Pages/Test.vue
@@ -0,0 +1,15 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+import Welcome from '@/Components/Welcome.vue';
+</script>
+
+<template>
+    <AppLayout title="Dashboard">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Test
+            </h2>
+        </template>
+        
+    </AppLayout>
+</template>
